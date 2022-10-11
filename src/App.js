6223279e2eb8b507a86a836890c97aa589119991
ydnwil89 <42740538+ydnwil89@users.noreply.github.com>
2022-10-11 22:42:07 +0700

    Added user list component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users/index';
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [users, setUsers] = useState([]);
+  const [invites, setInvites] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [success, setSuccess] = useState(false);
+  const [searchValue, setSearchValue] = useState('')
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then(res => res.json())
+      .then(json => {
+        setUsers(json.data);
+      })
+      .catch(err => {
+        console.warn(err);
+        alert('Ошибка при получении пользователей')
+      })
+      .finally(
+        () => setLoading(false)
+      );
+  }, []); 
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites(prev => prev.filter(_id => _id !== id));
+    } else {
+      setInvites(prev => [...prev, id]);
+    }
+  };
+
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  };
 
   return (
-    <div className='App'>
-      <div>
-        <h2>Counter</h2>
-        <h1>{counter}</h1>
-        <button onClick={() => setCounter(counter - 1)} className='minus'>-</button>
-        <button onClick={() => setCounter(counter + 1)} className='plus'>+</button>
-      </div>
+    <div className="App">
+      {
+        success ? (
+          <Success count={invites.length} /> 
+        ) : (
+          <Users 
+            onChangeSearchValue={onChangeSearchValue} 
+            searchValue={searchValue} 
+            items={users} 
+            isLoading={isLoading}
+            invites={invites}
+            onClickInvite={onClickInvite}
+            onClickSendInvites={onClickSendInvites}
+          />
+      )}
     </div>
   );
-}
+};
 
 export default App;
